4a3811265af9f1779ab041bec882d551cbf9a5b6
Eshwar <156096317+Eshwar-Thatikonda@users.noreply.github.com>
2025-07-17 14:53:41 +0530

    first program in java

---
+++ Programs/Hello.java
@@ -0,0 +1,6 @@
+class Hello {
+    public static void main(String args[]) {
+        System.out.println("Hello ! This is my first Java Program for the Start.");
+        System.console().printf("Hello");
+    }
+}
